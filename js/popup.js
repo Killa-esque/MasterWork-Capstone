1d8f49866b1cd0760bdcca816217205236101368
Vo Phu Vinh <phuvinh113@gmail.com>
2022-10-19 10:23:22 +0700

    commit

---
+++ js/popup.js
@@ -0,0 +1,6 @@
+function openModal() {
+  document.querySelector("#videoModal").style.display = "block";
+}
+function closeModal() {
+  document.querySelector("#videoModal").style.display = "none";
+}
